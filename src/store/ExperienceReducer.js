a53ecfb173bc8ef502d9dd5c90d0c1ecf7853d7f
Nikita Gonohov <gonohovn@gmail.com>
2024-03-28 16:55:36 +0200

    update

--- src/store/ExperienceReducer.js
+++ src/store/ExperienceReducer.js
@@ -28,19 +28,19 @@ const defaultState = {
         {
             id: 1,
             lang: 'ru',
-            date: 'Июнь 2023 — Все еще',
+            date: 'Май 2023 — Все еще',
             description: 'Занимался созданием проектов и веб-сайтов на фреймворке Laravel, с использованием языка программирования PHP в качестве Backend и JavaScript в качестве Frontend, но помимо этого имел дело с React, Redux фреймворками. Принимал участие в разработке множество сайтов, интернет магазинов, порталов и других уникальных проектов. Работая в этой компании получил опыт в Backend разработке, в работе с базой данных, сервером, SEO оптимизацией и многое другое.',
         },
         {
             id: 1,
             lang: 'ua',
-            date: "Червень 2023 — Досі",
+            date: "Травень 2023 — Досі",
             description: "Займався створенням проектів і веб-сайтів на фреймворку Laravel, з використанням мови програмування PHP як Backend і JavaScript як Frontend, а також мав справу з фреймворками React, Redux. Брав участь у розробці безлічі сайтів, інтернет-магазинів, порталів та інших унікальних проектів. Працюючи в цій компанії, отримав досвід в Backend розробці, роботі з базою даних, сервером, SEO оптимізацією та багато іншого.",
         },
         {
             id: 1,
             lang: 'en',
-            date: "June 2023 — Still",
+            date: "May 2023 — Still",
             description: "Engaged in creating projects and websites on the Laravel framework, using PHP as Backend and JavaScript as Frontend, but also dealt with React, Redux frameworks. Participated in the development of numerous websites, online stores, portals, and other unique projects. Working in this company gained experience in Backend development, working with databases, servers, SEO optimization, and much more.",
         },
 
