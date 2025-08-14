426e77a4dbdbf34dbdce26578c6415ff425278ef
Nikita <nikita.gonohov.it@gmail.com>
2025-08-14 22:13:40 +0300

    update text & styles

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,81 +1,123 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "./Experience.scss";
 import "styles/components_styles/CardLine.scss";
-import SectionHead from 'components/SectionHead/SectionHead';
-import { useSelector } from 'react-redux';
-import CompanyCommon from 'svg/CompanyCommon';
-import { useTranslation } from 'react-i18next';
-import { useTranslationDataArray } from 'hooks/useTranslations';
+import SectionHead from "components/SectionHead/SectionHead";
+import { useSelector } from "react-redux";
+import CompanyCommon from "svg/CompanyCommon";
+import { useTranslation } from "react-i18next";
+import { useTranslationDataArray } from "hooks/useTranslations";
 
 const Experience = () => {
     const { t, i18n } = useTranslation();
-    const companiesCommon = useSelector(state => state.ExperienceReducer.companies);
-    const companies_translations = useSelector(state => state.ExperienceReducer.companies_translations);
-    
-    const companies = useTranslationDataArray(companiesCommon, companies_translations, i18n.language)
+    const companiesCommon = useSelector(
+        (state) => state.ExperienceReducer.companies
+    );
+    const companies_translations = useSelector(
+        (state) => state.ExperienceReducer.companies_translations
+    );
+
+    const companies = useTranslationDataArray(
+        companiesCommon,
+        companies_translations,
+        i18n.language
+    );
 
     return (
-        <section className='experience_section container_box' id='experience'>
+        <section className="experience_section container_box" id="experience">
             <SectionHead
-                title={t('experience.head.title')}
-                subtitle={t('experience.head.subtitle')}
+                title={t("experience.head.title")}
+                subtitle={t("experience.head.subtitle")}
             />
             <div className="cards_line">
-                {
-                    companies.map(company =>
-                        <div key={company.id} className="experience_item">
-                            <div className="info_card">
-                                <div className="head">
-                                    <a href={company.href} target='_blank' className="logo">
-                                        {
-                                            company.logo
-                                                ?
-                                                <img src={require(`assets/companies/${company.logo}`)} alt={company.logo_alt} />
-                                                :
-                                                <CompanyCommon />
-                                        }
+                {companies.map((company) => (
+                    <div key={company.id} className="experience_item">
+                        <div className="info_card">
+                            <div className="head">
+                                <a
+                                    href={company.href}
+                                    target="_blank"
+                                    className="logo"
+                                >
+                                    {company.logo ? (
+                                        <img
+                                            src={require(`assets/companies/${company.logo}`)}
+                                            alt={company.logo_alt}
+                                        />
+                                    ) : (
+                                        <CompanyCommon />
+                                    )}
+                                </a>
+                                <div className="flex column">
+                                    <span className="job_title">
+                                        {company.job_title}
+                                    </span>
+                                    <a
+                                        href={company.href}
+                                        target="_blank"
+                                        className="company_name"
+                                    >
+                                        {company.company_name}
                                     </a>
-                                    <div className='flex column'>
-                                        <span className="job_title">{company.job_title}</span>
-                                        <a href={company.href} target='_blank' className="company_name">
-                                            {company.company_name}
-                                        </a>
-                                        <span className="date">{company.translation.date}</span>
-                                    </div>
+                                    <span className="date">
+                                        {company.translation.date}
+                                    </span>
                                 </div>
-                                <p className="description">{company.translation.description}</p>
-                                <div className="skills">
-                                    <b>{t('nav.skills')}:</b>
-                                    <ul className='undecorated_ul'>
-                                        {
-                                            company.skills.map((skill, index) =>
-                                                <li key={index}>{skill}</li>
+                            </div>
+                            <p className="description">
+                                {company.translation.description}
+                            </p>
+                            {company.translation.achievements.length > 0 && (
+                                <div className="achievements">
+                                    <b>{t("experience.achievements")}:</b>
+                                    <ul className="undecorated_ul">
+                                        {company.translation.achievements.map(
+                                            (achievement, index) => (
+                                                <li key={index}>
+                                                    {achievement}
+                                                </li>
                                             )
-                                        }
+                                        )}
                                     </ul>
                                 </div>
-                                <div className="logo-date hide flex-if_lg_size">
-                                    <span className="date">{company.translation.date}</span>
-                                </div>
+                            )}
+                            <div className="skills">
+                                <b>{t("nav.skills")}:</b>
+                                <ul className="undecorated_ul">
+                                    {company.skills.map((skill, index) => (
+                                        <li key={index}>{skill}</li>
+                                    ))}
+                                </ul>
                             </div>
-                            <div className="logo-date flex">
-                                <a href={company.href} target='_blank' className="logo_wrapper">
-                                    {
-                                        company.logo
-                                            ?
-                                            <img src={require(`assets/companies/${company.logo}`)} alt={company.logo_alt} />
-                                            :
-                                            <CompanyCommon />
-                                    }
-                                </a>
-                                <span className="date hide-if_lg_size">{company.translation.date}</span>
+                            <div className="logo-date hide flex-if_lg_size">
+                                <span className="date">
+                                    {company.translation.date}
+                                </span>
                             </div>
                         </div>
-                    )
-                }
+                        <div className="logo-date flex">
+                            <a
+                                href={company.href}
+                                target="_blank"
+                                className="logo_wrapper"
+                            >
+                                {company.logo ? (
+                                    <img
+                                        src={require(`assets/companies/${company.logo}`)}
+                                        alt={company.logo_alt}
+                                    />
+                                ) : (
+                                    <CompanyCommon />
+                                )}
+                            </a>
+                            <span className="date hide-if_lg_size">
+                                {company.translation.date}
+                            </span>
+                        </div>
+                    </div>
+                ))}
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default Experience;
--- src/store/ExperienceReducer.js
+++ src/store/ExperienceReducer.js
@@ -2,35 +2,63 @@ const defaultState = {
     companies: [
         {
             id: 1,
-            href: 'https://it-transformation.com.ua/',
-            logo: 'it_transformation.jpg',
-            logo_alt: 'it_transformation_logo',
-            job_title: 'Frontend Engineer Intern',
-            company_name: 'IT Transformation',
+            href: "https://it-transformation.com.ua/",
+            logo: "it_transformation.jpg",
+            logo_alt: "it_transformation_logo",
+            job_title: "Frontend Engineer Intern",
+            company_name: "IT Transformation",
             skills: [
-                'Vue', 'Vuex', 'Vuetify', 'HTML', 'CSS', 'JavaScript', 'Keycloak'
+                "Vue",
+                "Vuex",
+                "Vuetify",
+                "HTML",
+                "CSS",
+                "JavaScript",
+                "Keycloak",
             ],
         },
         {
             id: 2,
-            href: 'https://spekter.site/',
-            logo: 'spekter.jpg',
-            logo_alt: 'spekter_logo',
-            job_title: 'Junior Full Stack developer',
-            company_name: 'Spekter',
+            href: "https://spekter.site/",
+            logo: "spekter.jpg",
+            logo_alt: "spekter_logo",
+            job_title: "Junior Full Stack developer",
+            company_name: "Spekter",
             skills: [
-                'Laravel', 'PHP', 'MySQL', 'ReactJS', 'Redux', 'HTML', 'CSS', 'JavaScript', 'Sass', 'jQuery'
+                "Laravel",
+                "PHP",
+                "MySQL",
+                "ReactJS",
+                "Redux",
+                "HTML",
+                "CSS",
+                "JavaScript",
+                "Sass",
+                "jQuery",
             ],
         },
         {
             id: 3,
-            href: 'https://yeducoders.com/',
-            logo: 'yeDuCoders.jpg',
-            logo_alt: 'yellow-duck-coders_logo',
-            job_title: 'Full Stack developer',
-            company_name: 'Yellow Duck Coders',
+            href: "https://yeducoders.com/",
+            logo: "yeDuCoders.jpg",
+            logo_alt: "yellow-duck-coders_logo",
+            job_title: "Full Stack developer",
+            company_name: "Yellow Duck Coders",
             skills: [
-                'JavaScript', 'Laravel', 'PHP', 'PostgreSQL', 'Vue', 'Pinia', 'Vuetify', 'TypeScript', 'React', 'Tailwind', 'Sass', 'Vite', 'SQL', 'Node.js'
+                "JavaScript",
+                "Laravel",
+                "PHP",
+                "PostgreSQL",
+                "Vue",
+                "Pinia",
+                "Vuetify",
+                "TypeScript",
+                "React",
+                "Tailwind",
+                "Sass",
+                "Vite",
+                "SQL",
+                "Node.js",
             ],
         },
     ],
@@ -39,72 +67,119 @@ const defaultState = {
         {
             id: 1,
             companyId: 1,
-            lang: 'ru',
-            date: 'Декабрь 2022 - Март 2023',
-            description: 'Проходил стажировку в качестве Frontend разработчка, принимал участие в создании веб-приложений и сайтов. В качестве основного фреймворка был выбран Vue, кроме этого имел дело с такими технологиями как Vuex, Vuetify, JavaScript и многое другое. Также получил уникальный опыт в работе с авторизацией с использованием системы управления идентификацией и доступом - Keycloak.',
+            lang: "ru",
+            date: "Декабрь 2022 - Март 2023",
+            description:
+                "Участвовал в разработке веб-приложений с использованием Vue и Vuetify. Реализовал авторизацию пользователей через Keycloak.",
+            achievements: [],
         },
         {
             id: 2,
             companyId: 1,
-            lang: 'ua',
+            lang: "ua",
             date: "Грудень 2022 - Березень 2023",
-            description: 'Проходив стажування як Frontend розробник, брав участь у створенні веб-додатків та сайтів. В якості фреймворка був обраний Vue, крім цього мав справу з такими технологіями як Vuex, Vuetify, JavaScript та багато іншого. Також отримав унікальний досвід у роботі з авторизацією з використанням системи керування ідентифікацією та доступом – Keycloak.'
+            description:
+                "Брав участь у розробці веб-застосунків з використанням Vue та Vuetify. Реалізував авторизацію користувачів через Keycloak.",
+            achievements: [],
         },
         {
             id: 3,
             companyId: 1,
-            lang: 'en',
+            lang: "en",
             date: "December 2022 - March 2023",
-            description: 'I completed an internship as a Frontend developer and took part in the creation of web applications and websites. Vue was chosen as the main framework, but in addition I dealt with technologies such as Vuex, Vuetify, JavaScript and much more. I also gained unique experience in working with authorization using the identity and access management system - Keycloak.'
+            description:
+                "Participated in the development of web applications using Vue and Vuetify. Implemented user authorization via Keycloak.",
+            achievements: [],
         },
+
         {
             id: 4,
             companyId: 2,
-            lang: 'ru',
-            date: 'Май 2023 — Июнь 2024',
-            description: 'Занимался созданием проектов и веб-сайтов на фреймворке Laravel, с использованием языка программирования PHP в качестве Backend и JavaScript в качестве Frontend, но помимо этого имел дело с React, Redux фреймворками. Принимал участие в разработке множество сайтов, интернет магазинов, порталов и других уникальных проектов. Работая в этой компании получил опыт в Backend разработке, в работе с базой данных, сервером, SEO оптимизацией и многое другое.',
+            lang: "ru",
+            date: "Май 2023 - Июнь 2024",
+            description:
+                "Занимался разработкой и поддержкой веб-сайтов и проектов на Laravel. Принимал участие во фронтенд и в бэкенд разработке, интеграции с базами данных и настройке серверов.",
+            achievements: [
+                "Реализовал функционал на Laravel для нескольких коммерческих проектов.",
+                "Оптимизировал работу сайтов, улучшив скорость загрузки и SEO.",
+                "Применил React и Redux для создания интерактивных интерфейсов.",
+            ],
         },
         {
             id: 5,
             companyId: 2,
-            lang: 'ua',
-            date: "Травень 2023 — Червень 2024",
-            description: "Займався створенням проектів і веб-сайтів на фреймворку Laravel, з використанням мови програмування PHP як Backend і JavaScript як Frontend, а також мав справу з фреймворками React, Redux. Брав участь у розробці безлічі сайтів, інтернет-магазинів, порталів та інших унікальних проектів. Працюючи в цій компанії, отримав досвід в Backend розробці, роботі з базою даних, сервером, SEO оптимізацією та багато іншого.",
+            lang: "ua",
+            date: "Травень 2023 - Червень 2024",
+            description:
+                "Працював над розробкою та підтримкою веб-сайтів і проектів на Laravel. Брав участь у фронтенд та бекенд розробці, інтеграції з базами даних та налаштуванні серверів.",
+            achievements: [
+                "Реалізував функціонал на Laravel для кількох комерційних проектів.",
+                "Оптимізував роботу сайтів, покращивши швидкість завантаження та SEO.",
+                "Застосував React і Redux для створення інтерактивних інтерфейсів.",
+            ],
         },
         {
             id: 6,
             companyId: 2,
-            lang: 'en',
-            date: "May 2023 — June 2024",
-            description: "I engaged in creating projects and websites on the Laravel framework, using PHP as Backend and JavaScript as Frontend, but also dealt with React, Redux frameworks. Participated in the development of numerous websites, online stores, portals, and other unique projects. Working in this company gained experience in Backend development, working with databases, servers, SEO optimization, and much more.",
+            lang: "en",
+            date: "May 2023 - June 2024",
+            description:
+                "Worked on developing and supporting websites and projects on Laravel. Participated in frontend and backend development, database integration and server setup.",
+            achievements: [
+                "Implemented functionality on Laravel for several commercial projects.",
+                "Optimized website performance, improving loading speed and SEO.",
+                "Used React and Redux to create interactive interfaces.",
+            ],
         },
+
         {
             id: 7,
             companyId: 3,
-            lang: 'ru',
-            date: 'Июнь 2024 — Все еще',
-            description: "Работал в крупной международной компании, где был частью одной из многочисленных команд. Основным проектом на протяжении года была разработка масштабной ERP-системы, с гибкой архитектурой, использованием CI/CD и code review. Во время работы над этим проектом я значительно прокачал как технические, так и командные навыки: принимал участие в созвонах с заказчиком, активно взаимодействовал с другими командами, углубил понимание микроменеджмента и управления задачами, улучшил навыки коммуникации, планирования и быстрой адаптации в условиях меняющихся требований. Помимо основного проекта участвовал в поддержке других уже запущенных продуктов. Технологический стек включал как знакомые инструменты (PHP, Laravel, JavaScript), так и новые для меня: PostgreSQL, TypeScript, Pinia, Tailwind, Vuetify, DBeaver, Postman и другие.",
+            lang: "ru",
+            date: "Июнь 2024 — Все еще",
+            description:
+                "Как член большой международной команды, я работал над разработкой масштабной ERP-системы с гибкой архитектурой, использованием CI/CD и процессом code-review. Участвовал в ежедневных митингах с заказчиком и сотрудничал с другими командами, обеспечивая качественную интеграцию модулей и стабильность продукта. Также занимался поддержкой уже запущенных продуктов.",
+            achievements: [
+                "Успешно реализовал ключевые модули ERP-системы, что оптимизировали бизнес-процессы клиента.",
+                "Углубил экспертизу в планировании и менеджменте задач, работая в условиях часто изменяющихся требований.",
+                "Наладил эффективное взаимодействие с другими командами, что сократило время согласования и интеграции.",
+                "Поддержал и усовершенствовал несколько существующих продуктов, повысив их стабильность и производительность.",
+            ],
         },
         {
             id: 8,
             companyId: 3,
-            lang: 'ua',
-            date: "Червень 2024 — Досі",
-            description: "Працював у великій міжнародній компанії, де був частиною однієї з численних команд. Основним проектом протягом року була розробка масштабної ERP-системи з гнучкою архітектурою, використанням CI/CD та code review. Під час роботи над цим проектом я значно прокачав як технічні, так і командні навички: брав участь у дзвонах із замовником, активно взаємодіяв з іншими командами, поглибив розуміння мікроменеджменту та управління завданнями, покращив навички комунікації, планування та швидкої адаптації в умовах змінних вимог. Крім основного проекту, брав участь у підтримці інших вже запущених продуктів. Технологічний стек включав як знайомі інструменти (PHP, Laravel, JavaScript), так і нові для мене: PostgreSQL, TypeScript, Pinia, Tailwind, Vuetify, DBeaver, Postman та інші.",
+            lang: "ua",
+            date: "Червень 2024 - Досі",
+            description:
+                "Як член великої міжнародної команди, я працював над розробкою масштабної ERP-системи з гнучкою архітектурою, використанням CI/CD та процесом code-review. Брав участь у щоденних мітингах із замовником та співпрацював з іншими командами, забезпечуючи якісну інтеграцію модулів і стабільність продукту. Також займався підтримкою вже запущених продуктів.",
+            achievements: [
+                "Успішно реалізував ключові модулі ERP-системи, що оптимізували бізнес-процеси клієнта.",
+                "Поглибив експертизу в плануванні та менеджменті задач, працюючи в умовах часто змінюваних вимог.",
+                "Налагодив ефективну взаємодію з іншими командами, що скоротило час узгодження та інтеграції.",
+                "Підтримав і вдосконалив кілька існуючих продуктів, підвищивши їхню стабільність та продуктивність.",
+            ],
         },
         {
             id: 9,
             companyId: 3,
-            lang: 'en',
-            date: "June 2024 — Still",
-            description: "I worked at a large international company as part of one of many teams. My main focus for a year was contributing to the development of a large-scale ERP system with a flexible architecture, CI/CD pipelines, and regular code reviews. Throughout this project, I significantly improved both my technical and soft skills: I participated in client calls, collaborated actively with other teams, deepened my understanding of micromanagement and task coordination, and enhanced my communication, planning, and adaptability in the face of changing requirements. In addition to the main project, I was also involved in maintaining and supporting other live products. The tech stack included familiar tools (PHP, Laravel, JavaScript) as well as new ones I mastered during the process: PostgreSQL, TypeScript, Pinia, Tailwind, Vuetify, DBeaver, Postman and more.",
+            lang: "en",
+            date: "June 2024 - Still",
+            description:
+                "As a member of a large international team, I worked on the development of a large-scale ERP system with a flexible architecture, using CI/CD and a code review process. Participated in daily meetings with the customer and collaborated with other teams, ensuring high-quality integration of modules and product stability. Also supported already launched products.",
+            achievements: [
+                "Successfully implemented key modules of the ERP system, which optimized the client's business processes.",
+                "Deepened my expertise in planning and task management, working in conditions of frequently changing requirements.",
+                "Established effective interaction with other teams, which reduced the time of approval and integration.",
+                "Supported and improved several existing products, increasing their stability and performance.",
+            ],
         },
-    ]
-}
+    ],
+};
 
 export const ExperienceReducer = (state = defaultState, action) => {
     switch (action.type) {
         default:
-            return state
+            return state;
     }
-}
+};
